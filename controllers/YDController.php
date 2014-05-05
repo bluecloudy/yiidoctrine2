7baaae120a36d059e7014b9f3aed3cb917af1d8d
Giang Nguyen <tn.hoanggiang@gmail.com>
2014-05-05 10:37:33 +0700

    Add extension files

---
+++ controllers/YDController.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * YiiDoctrine2Controller
+ *
+ * @author    Giang Nguyen (tn.hoanggiang@gmail.com)
+ * @link      http://smartexts.com
+ * @copyright Copyright &copy; 2014, smartexts.com.
+ * @license   https://github.com/bluecloudy/yiidoctrine2
+ */
+class YiiDoctrine2Controller extends CController
+{
+	/**
+	 * @var Doctrine\ORM\EntityManager $entityManager
+	 */
+	private $entityManager = null;
+
+	/**
+	 * @return CHttpRequest
+	 */
+	public function getRequest()
+	{
+		return Yii::app()->request;
+	}
+
+	/**
+	 * @return BaseRepository
+	 */
+	public function getRepository()
+	{
+		$class = get_class($this);
+		return $this->getEntityManager()->getRepository(substr($class, 0, (strlen($class) - 10)));
+	}
+
+	/**
+	 * @return Doctrine\ORM\EntityManager
+	 */
+	public function getEntityManager()
+	{
+		if (is_null($this->entityManager))
+		{
+			$this->entityManager = Yii::app()->doctrine->getEntityManager();
+		}
+		return $this->entityManager;
+	}
+}
